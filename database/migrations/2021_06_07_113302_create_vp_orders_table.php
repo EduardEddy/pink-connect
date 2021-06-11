f617a857a2a341105a1dcdd0fb809521e0defdd7
reyes98 <34418186+reyes98@users.noreply.github.com>
2021-06-11 06:56:56 -0500

    order migrations

---
+++ database/migrations/2021_06_07_113302_create_vp_orders_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVpOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vp_orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('marketplaceName')->nullable();
+            $table->string('marketplaceCode')->nullable();
+            $table->integer('marketplaceOrderCode')->nullable();
+            $table->integer("shopChannelId")->nullable();
+            $table->string('shopChannelName')->nullable();
+            $table->enum('status', ['WAITING_ACCEPTANCE','PENDING', 'PROCESSING', 'SHIPPED','CANCELLED'])->default('WAITING_ACCEPTANCE');
+            $table->timestamp('shippedOrderDate')->nullable();
+            $table->integer('totalPice')->nullable();
+            $table->integer('shippingCosts')->nullable();
+            $table->integer('shippingTaxRate')->nullable();
+            $table->string('currency')->nullable();
+            $table->string('requestedShippingMethod')->nullable();
+            $table->string('deliveryNote')->nullable();
+            $table->string('pickupPointId')->nullable();
+            $table->timestamps();
+            $table->boolean('updated')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vp_orders');
+    }
+}
---
+++ database/migrations/2021_06_10_154545_create_vp_order_delivery_details_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVpOrderDeliveryDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vp_order_delivery_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('order_id');
+            $table->string("carrierId")->nullable();
+            $table->string("carrierName")->nullable();
+            $table->string("trackingNumber")->nullable();
+            $table->string("trackingUrl")->nullable();
+            $table->timestamps();
+            $table->foreign('order_id')->references('id')->on('vp_orders')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vp_order_delivery_details');
+    }
+}
